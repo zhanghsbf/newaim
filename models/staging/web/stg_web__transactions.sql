42b8a6ea3373855b3da543ddc86cc8d7d47425fe
zhangyunkun <xiaozhangchiren@sina.com>
2024-02-27 21:53:27 +0800

    交易表接入数据时过滤一天前的数据

--- models/staging/web/stg_web__transactions.sql
+++ models/staging/web/stg_web__transactions.sql
@@ -12,8 +12,15 @@ renamed as (
         item_count,
         category, 
         _loaded_at_utc
-    from source     
+    from source    
+),
+
+clean_older as (
+    select * from renamed 
+--    where transaction_date >= (current_date - 1)  过滤超过1天的数据
+    where transaction_date >= (to_date('2023-04-12','yyyy-MM-dd') - 1)  --测试用,假设跑数日期为2023-04-12
+    and transaction_date < to_date('2023-04-12','yyyy-MM-dd')
 )
 
-select * from renamed
+select * from clean_older
 
